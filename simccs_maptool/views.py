5cebf5aa00a5710706c5138ed24b2447fed22e0e
Marcus Christie <machrist@iu.edu>
2019-05-08 16:38:10 -0400

    Update mps generation to write full scenario dir to user storage

--- simccs_maptool/views.py
+++ simccs_maptool/views.py
@@ -46,15 +46,41 @@ def simccs(request):
 
 @login_required
 def generate_mps(request):
-    with tempfile.TemporaryDirectory(dir=SCENARIOS_DIRPATH) as scenario_dir:
-        scenario = os.path.basename(scenario_dir)
-        # Write Sources.txt
-        os.mkdir(os.path.join(scenario_dir, "Sources"))
-        with open(
-            os.path.join(scenario_dir, "Sources", "Sources.txt"), mode="w"
-        ) as sources:
-            sources.write(
-                """ID	costFix ($M)	fixO&M ($M/y)	varO&M ($/tCO2)	capMax (MtCO2/y)	N/A	LON	LAT	NAME
+    # TODO: provide Django apps with utility for writing to gateway data storage
+    experiment_data_storage = FileSystemStorage(
+        location=settings.GATEWAY_DATA_STORE_DIR
+    )
+    userdir = os.path.join(
+        settings.GATEWAY_DATA_STORE_DIR,
+        experiment_data_storage.get_valid_name(request.user.username),
+    )
+    datasets_basepath = os.path.join(userdir, "Datasets")
+    dataset_dir = os.path.join(datasets_basepath, SOUTHEASTUS_DATASET)
+    os.makedirs(dataset_dir, exist_ok=True)
+    # Symlink in the BaseData directory for the dataset
+    basedata_dir = os.path.join(dataset_dir, "BaseData")
+    if not os.path.exists(basedata_dir):
+        os.symlink(
+            os.path.join(DATASETS_BASEPATH, SOUTHEASTUS_DATASET, "BaseData"),
+            basedata_dir
+        )
+    # Create a scenario directory
+    scenarios_dir = os.path.join(dataset_dir, "Scenarios")
+    os.makedirs(scenarios_dir, exist_ok=True)
+    scenario_dir = os.path.join(
+        scenarios_dir,
+        "scenario_{}".format(datetime.utcnow().strftime("%Y%m%dT%H%M%SZ")),
+    )
+    os.mkdir(scenario_dir)
+
+    scenario = os.path.basename(scenario_dir)
+    # Write Sources.txt
+    os.mkdir(os.path.join(scenario_dir, "Sources"))
+    with open(
+        os.path.join(scenario_dir, "Sources", "Sources.txt"), mode="w"
+    ) as sources:
+        sources.write(
+            """ID	costFix ($M)	fixO&M ($M/y)	varO&M ($/tCO2)	capMax (MtCO2/y)	N/A	LON	LAT	NAME
 1	2842.8	223.4	81.93	7.236	1	-88.0103	31.0069	1
 2	3052.4	234.5	58	7.236	1	-86.4567	33.2442	2
 3	407.9	52.5	106.78	0.297	1	-85.9708	34.0128	3
@@ -76,12 +102,12 @@ def generate_mps(request):
 19	1405.6	102.3	68.72	2.529	1	-89.0265	30.4408	19
 20	2091	128.6	89.68	5.013	1	-88.5574	30.5335	20
 """
-            )
-        # Write Sinks.txt
-        os.mkdir(os.path.join(scenario_dir, "Sinks"))
-        with open(os.path.join(scenario_dir, "Sinks", "Sinks.txt"), mode="w") as sinks:
-            sinks.write(
-                """0	1	2	3	4	5	6	7	8	9	10	11	12	13	14	15	16
+        )
+    # Write Sinks.txt
+    os.mkdir(os.path.join(scenario_dir, "Sinks"))
+    with open(os.path.join(scenario_dir, "Sinks", "Sinks.txt"), mode="w") as sinks:
+        sinks.write(
+            """0	1	2	3	4	5	6	7	8	9	10	11	12	13	14	15	16
 1	1	379	40.951	0.000	0.375	4.575	0.175	2.61	0	-91.2980	28.9920	0	0	0	0	Gulf offshore
 2	2	379	40.951	0.000	0.375	4.575	0.175	2.61	0	-91.1764	31.5559	0	0	0	0	Cranfield
 3	3	360	63.063	0.000	0.180	3.859	0.201	3.02	0	-88.5068	30.5078	0	0	0	0	Escatawpa
@@ -90,24 +116,24 @@ def generate_mps(request):
 6	6	549	63.063	0.000	0.600	3.017	0.154	2.83	0	-84.8449	30.6128	0	0	0	0	Disposal Area 2 (DA1b)
 7	7	845	63.063	0.000	0.800	2.930	0.135	2.77	0	-81.6781	31.7306	0	0	0	0	Disposal Area 3 (DA2)
 """
-            )
-        # Write Linear.txt
-        os.mkdir(os.path.join(scenario_dir, "Transport"))
-        with open(
-            os.path.join(scenario_dir, "Transport", "Linear.txt"), mode="w"
-        ) as linear:
-            linear.write(
-                """ID	X (Con)	C (Con)	X (ROW)	C (ROW)
+        )
+    # Write Linear.txt
+    os.mkdir(os.path.join(scenario_dir, "Transport"))
+    with open(
+        os.path.join(scenario_dir, "Transport", "Linear.txt"), mode="w"
+    ) as linear:
+        linear.write(
+            """ID	X (Con)	C (Con)	X (ROW)	C (ROW)
 1	0.0762	0.1789	0.0069	0.1174
 2	0.0162	0.4932	0.0009	0.1511
 """
-            )
+        )
 
         try:
             from jnius import autoclass
 
             DataStorer = autoclass("simccs.dataStore.DataStorer")
-            data = DataStorer(DATASETS_BASEPATH, SOUTHEASTUS_DATASET, scenario)
+            data = DataStorer(datasets_basepath, SOUTHEASTUS_DATASET, scenario)
             Solver = autoclass("simccs.solver.Solver")
             solver = Solver(data)
             data.setSolver(solver)
@@ -119,7 +145,7 @@ def generate_mps(request):
                 0.1,
                 30,
                 15,
-                DATASETS_BASEPATH,
+                datasets_basepath,
                 SOUTHEASTUS_DATASET,
                 scenario,
             )
@@ -128,24 +154,12 @@ def generate_mps(request):
                 "Error occurred when calling writeMPS: " + str(e.stacktrace)
             )
 
-        # copy <scenario_dir>/MIP/mips.mps to
-        # GATEWAY_DATA_STORE_DIR/<username>/MIP_Files/mip_<timestamp>.mps
-        source_mps_file_path = os.path.join(scenario_dir, "MIP", "mip.mps")
-        # TODO: provide Django apps with utility for writing to gateway data storage
-        experiment_data_storage = FileSystemStorage(
-            location=settings.GATEWAY_DATA_STORE_DIR
-        )
-        mps_file_name = experiment_data_storage.get_valid_name(
-            "mip_{}.mps".format(datetime.utcnow().replace(microsecond=0).isoformat())
-        )
-        user_mps_file_path = os.path.join("MIP_Files", mps_file_name)
-        dest_mps_file_path = os.path.join(
-            experiment_data_storage.get_valid_name(request.user.username),
-            user_mps_file_path,
-        )
-        with open(source_mps_file_path) as source_mps_file:
-            experiment_data_storage.save(dest_mps_file_path, source_mps_file)
-        return JsonResponse({"user_file": user_mps_file_path})
+        mps_file_path = os.path.join(scenario_dir, "MIP", "mip.mps")
+        rel_mps_file_path = os.path.relpath(mps_file_path, start=userdir)
+        results_dir = os.path.join(scenario_dir, "Results")
+        os.mkdir(results_dir)
+        rel_results_dir = os.path.relpath(results_dir, start=userdir)
+        return JsonResponse({"user_file": rel_mps_file_path, "results_dir": rel_results_dir})
 
 
 def candidate_network(request):
