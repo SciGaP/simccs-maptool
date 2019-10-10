b89b95b8f8c36560c56a6d1444daaa4206293206
Marcus Christie <machrist@iu.edu>
2019-10-10 18:29:38 -0400

    Reducing max memory for Java calls

--- simccs_maptool/apps.py
+++ simccs_maptool/apps.py
@@ -51,7 +51,7 @@ def init_pyjnius():
         logger.info("JAVA_HOME set to '{}'".format(os.environ["JAVA_HOME"]))
         BASE_DIR = os.path.dirname(os.path.abspath(__file__))
         jnius_config.add_options('-Djava.awt.headless=true')
-        jnius_config.add_options('-Xmx2G')
+        jnius_config.add_options('-Xmx1G')
         jnius_config.set_classpath(
             os.path.join(
                 BASE_DIR, "simccs", "lib", "simccs-app-1.0-jar-with-dependencies.jar"
