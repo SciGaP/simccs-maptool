931cedb8eaf63f7e4c90615a5a6634f6c3fbd537
Marcus Christie <machrist@iu.edu>
2020-11-05 10:42:59 -0500

    #80: rename djangorestframework deprecated base_name to basename

--- simccs_maptool/urls.py
+++ simccs_maptool/urls.py
@@ -20,8 +20,8 @@ from rest_framework import routers
 from . import geoserver, views
 
 router = routers.DefaultRouter()
-router.register(r"cases", views.CaseViewSet, base_name="case")
-router.register(r"datasets", views.DatasetViewSet, base_name="dataset")
+router.register(r"cases", views.CaseViewSet, basename="case")
+router.register(r"datasets", views.DatasetViewSet, basename="dataset")
 app_name = "simccs_maptool"
 urlpatterns = [
     url(r"^$", views.HomeView.as_view(), name="home"),
