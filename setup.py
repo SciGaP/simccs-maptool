9fa63e8f627aa935c8c223d2775027bd2fbe2c5d
Marcus Christie <marc.christie@gmail.com>
2023-09-28 17:18:26 -0400

    Add support for building JS code when installed with pip

--- setup.py
+++ setup.py
@@ -1,58 +1,58 @@
 import distutils
+import logging
 import os
 import subprocess
 
 import setuptools
-from setuptools.command.develop import develop
-from setuptools.command.install import install
 
 
 def build_js():
-    subprocess.check_call(["yarn", "install"], cwd=os.path.join(os.getcwd(), "frontend"))
-    subprocess.check_call(["yarn", "run", "build"], cwd=os.path.join(os.getcwd(), "frontend"))
+    subprocess.check_call(["yarn", "install"],
+                          cwd=os.path.join(os.getcwd(), "frontend"))
+    subprocess.check_call(["yarn", "run", "build"],
+                          cwd=os.path.join(os.getcwd(), "frontend"))
 
 
-# Build JS code when this package is installed in virtual env
-# https://stackoverflow.com/a/36902139
-class BuildJSDevelopCommand(develop):
-    def run(self):
-        self.announce("Building JS code", level=distutils.log.INFO)
-        build_js()
-        super().run()
+try:
+    from setuptools.command.build import build as _build
+    from setuptools.command.editable_wheel import \
+        editable_wheel as _editable_wheel
 
+    class build(_build):
+        def run(self) -> None:
+            self.announce("Building JS code", level=logging.INFO)
+            build_js()
+            super().run()
 
-class BuildJSInstallCommand(install):
-    def run(self):
-        self.announce("Building JS code", level=distutils.log.INFO)
-        build_js()
-        super().run()
+    class editable_wheel(_editable_wheel):
+        def run(self) -> None:
+            self.announce("Building JS code", level=logging.INFO)
+            build_js()
+            super().run()
 
+    cmdclass = dict(build=build, editable_wheel=editable_wheel)
 
-setuptools.setup(
-    name="simccs-maptool",
-    version="0.0.1",
-    description="SimCCS MapTool plugin to Airavata Django Portal",
-    packages=setuptools.find_packages(),
-    include_package_data=True,
-    install_requires=[
-        'django>=1.11.16',
-        'cython',
-        'pyjnius',
-        'pyshp',
-        "airavata-django-portal-sdk",
-        'djangorestframework',
-        # Pandas 1.2 requires Python 3.7+ and our deployments are currently on Python 3.6
-        'pandas<1.2',
-        'geopandas'
-    ],
-    entry_points="""
-[airavata.djangoapp]
-simccs_maptool = simccs_maptool.apps:MapToolConfig
-[airavata.output_view_providers]
-cplex-solution-link = simccs_maptool.output_views:SolutionLinkProvider
-""",
-    cmdclass={
-        'develop': BuildJSDevelopCommand,
-        'install': BuildJSInstallCommand,
-    }
-)
+except ImportError:
+
+    # For older versions of setuptools (Python 3.6)
+    from setuptools.command.develop import develop as _develop
+    from setuptools.command.install import install as _install
+
+    # Build JS code when this package is installed in virtual env
+    # https://stackoverflow.com/a/36902139
+
+    class develop(_develop):
+        def run(self):
+            self.announce("Building JS code", level=distutils.log.INFO)
+            build_js()
+            super().run()
+
+    class install(_install):
+        def run(self):
+            self.announce("Building JS code", level=distutils.log.INFO)
+            build_js()
+            super().run()
+
+    cmdclass = dict(develop=develop, install=install)
+
+setuptools.setup(cmdclass=cmdclass)
